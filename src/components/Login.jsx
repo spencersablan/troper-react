fc4cce936cadfac64aff951cc2212a80efae7864
Spencer Sablan <spencertsablan@gmail.com>
2022-06-08 22:14:21 -0700

    Google btn

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,7 @@
 import { signInWithGoogle } from "../utils/firebase";
 import { useAuth } from "../contexts/AuthContext";
 import { useNavigate } from "react-router-dom";
+import SvgGoogleIcon from "./SvgGoogleIcon";
 import "./Login.scss";
 
 export default function Login() {
@@ -14,9 +15,12 @@ export default function Login() {
 	};
 
 	return (
-		<div className="sign-in">
-			<h3 className="sign-in--header">Login</h3>
-			<button onClick={googleLogin}>Sign In with Google</button>
+		<div className="login">
+			<h3 className="login--header">Login</h3>
+			<button className="login--button" onClick={googleLogin}>
+				<SvgGoogleIcon />
+				<span>Sign in with Google</span>
+			</button>
 		</div>
 	);
 }
